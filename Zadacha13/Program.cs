476c0114e906cebc44e22b8d59b2f6a06df5772b
Evgenuu Sakin <shve-ster@yandex.ru>
2022-09-23 19:55:47 +0300

    second seminar

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string anyNumber = Convert.ToString(number);
+if (anyNumber.Length > 2){
+  Console.WriteLine("Третья цифра этого числа: " + anyNumber[2]);
+}
+else {
+  Console.WriteLine("В этом числе нет третьей цифры ");
+}
---
+++ Zadacha15/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да 7 -> да 1 -> нет
+
+Console.WriteLine("Ввведите цифру от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+void Week(int number)
+{
+    if (number > 7 || number < 0)
+    {
+        Console.WriteLine("Неверно укзали цыфру дня недели ");
+    }
+    if (number == 1)
+    {
+        Console.WriteLine("Это понедельник - рабочий день! ");
+    }
+    if (number == 2)
+    {
+        Console.WriteLine("Это вторник - рабочий день! ");
+    }
+    if (number == 3)
+    {
+        Console.WriteLine("Это среда - рабочий день! ");
+    }
+    if (number == 4)
+    {
+        Console.WriteLine("Это четверг - рабочий день! ");
+    }
+    if (number == 5)
+    {
+        Console.WriteLine("Это пятница - рабочий день! ");
+    }
+    if (number == 6)
+    {
+        Console.WriteLine("Это суббота - является выходным днем! ");
+    }
+    if (number == 7)
+    {
+        Console.WriteLine("Это воскресение - является выходным днем! ");
+    }
+}
+Week(number);
+
+// расписал подробно, мог написать в соответствии с примерами ответов по этой задаче
